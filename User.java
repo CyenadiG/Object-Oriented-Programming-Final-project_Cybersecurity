5562abc4fb848c55c70759734c535db9f0b0497a
CyenadiG <114695306+CyenadiG@users.noreply.github.com>
2025-05-05 11:41:19 -0700

    Update User.java
    added setters and getters

--- User.java
+++ User.java
@@ -1,3 +1,11 @@
+/*
+Job: Represents a user in the system
+Task breakdown:
+    1.Stores user details like username, passwordHash, role, etc.
+Connection:
+    1. Used by AuthManager.java for managing user login, registration, and session tracking
+    2.Stored and retrieved in/from the database by UserDAO.java
+ */
 package model;
 
 public class User {
@@ -6,17 +14,40 @@ public class User {
     private String passwordHash;
     private String role;
 
+    //Getters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //Setters
     public void setId(int id) {
+        this.id = id;
     }
 
     public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPasswordHash(String passwordHash) {
+
     }
 
     public void setRole(String role) {
     }
 
-    // Constructors, Getters, Setters
 }
+
